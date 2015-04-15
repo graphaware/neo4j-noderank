297f2f4cd0f169c1bc0ab62b49e4a5cee7d81037
Michal Bachman <michal@graphaware.com>
2015-04-15 18:24:38 +0200

    latest fw

--- src/test/java/com/graphaware/module/noderank/NodeRankApiTest.java
+++ src/test/java/com/graphaware/module/noderank/NodeRankApiTest.java
@@ -1,12 +1,11 @@
 package com.graphaware.module.noderank;
 
 import com.graphaware.test.integration.NeoServerIntegrationTest;
-import static org.junit.Assert.*;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
 
-import static com.graphaware.test.util.TestUtils.executeCypher;
-import static com.graphaware.test.util.TestUtils.get;
+import static org.junit.Assert.assertTrue;
+
 
 public class NodeRankApiTest extends NeoServerIntegrationTest {
 
@@ -20,24 +19,24 @@ public class NodeRankApiTest extends NeoServerIntegrationTest {
 
     @Test
     public void shouldRetrieveTopNodes() throws InterruptedException {
-        executeCypher(baseUrl(), "CREATE (m:Person {name:'Michal'})-[:FRIEND_OF]->(d:Person {name:'Daniela'}),"+
-        " (m)-[:FRIEND_OF]->(v:Person {name:'Vojta'}),"+
-        " (m)-[:FRIEND_OF]->(a:Person {name:'Adam'}),"+
-        " (m)-[:FRIEND_OF]->(vi:Person {name:'Vince'}),"+
-        " (m)-[:FRIEND_OF]->(:Person {name:'Luanne'}),"+
-        " (vi)-[:FRIEND_OF]->(a),"+
-        " (d)-[:FRIEND_OF]->(a),"+
-        " (d)-[:FRIEND_OF]->(vi),"+
-        " (v)-[:FRIEND_OF]->(a)");
+        httpClient.executeCypher(baseUrl(), "CREATE (m:Person {name:'Michal'})-[:FRIEND_OF]->(d:Person {name:'Daniela'})," +
+                " (m)-[:FRIEND_OF]->(v:Person {name:'Vojta'})," +
+                " (m)-[:FRIEND_OF]->(a:Person {name:'Adam'})," +
+                " (m)-[:FRIEND_OF]->(vi:Person {name:'Vince'})," +
+                " (m)-[:FRIEND_OF]->(:Person {name:'Luanne'})," +
+                " (vi)-[:FRIEND_OF]->(a)," +
+                " (d)-[:FRIEND_OF]->(a)," +
+                " (d)-[:FRIEND_OF]->(vi)," +
+                " (v)-[:FRIEND_OF]->(a)");
 
         Thread.sleep(20000);
 
-        String s = get(baseUrl() + "/graphaware/noderank/noderank/", HttpStatus.OK.value());
+        String s = httpClient.get(baseUrl() + "/graphaware/noderank/noderank/", HttpStatus.OK.value());
         assertTrue(s.contains("{\"id\":0,\"labels\":[\"Person\"],\"name\":\"Michal\",\"nodeRank\""));
     }
 
     @Test
     public void requestToUnknownModuleShouldProduce404() throws InterruptedException {
-        get(baseUrl() + "/graphaware/noderank/unknown/",  HttpStatus.NOT_FOUND.value());
+        httpClient.get(baseUrl() + "/graphaware/noderank/unknown/",  HttpStatus.NOT_FOUND.value());
     }
 }
