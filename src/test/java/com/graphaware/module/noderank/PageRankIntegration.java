9f7beb0a73e72e584ac6b4376e36913ec8f5f944
Michal Bachman <michal@graphaware.com>
2015-11-21 13:20:43 +0800

    new framework, imports, copyright, logging

--- src/test/java/com/graphaware/module/noderank/PageRankIntegration.java
+++ src/test/java/com/graphaware/module/noderank/PageRankIntegration.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2013-2015 GraphAware
+ *
+ * This file is part of the GraphAware Framework.
+ *
+ * GraphAware Framework is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details. You should have received a copy of
+ * the GNU General Public License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 package com.graphaware.module.noderank;
 
 import com.graphaware.module.algo.generator.GraphGenerator;
@@ -7,7 +23,6 @@ import com.graphaware.module.algo.generator.config.BasicGeneratorConfig;
 import com.graphaware.module.algo.generator.node.SocialNetworkNodeCreator;
 import com.graphaware.module.algo.generator.relationship.BarabasiAlbertRelationshipGenerator;
 import com.graphaware.module.algo.generator.relationship.SocialNetworkRelationshipCreator;
-import com.graphaware.module.noderank.utils.PageRank;
 import com.graphaware.module.noderank.utils.*;
 import com.graphaware.runtime.GraphAwareRuntime;
 import com.graphaware.runtime.GraphAwareRuntimeFactory;
@@ -28,7 +43,7 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static java.util.Collections.sort;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Integration tests for page rank module.
--- src/test/java/com/graphaware/module/noderank/utils/RankNodePair.java
+++ src/test/java/com/graphaware/module/noderank/utils/RankNodePair.java
@@ -1,12 +1,26 @@
+/*
+ * Copyright (c) 2013-2015 GraphAware
+ *
+ * This file is part of the GraphAware Framework.
+ *
+ * GraphAware Framework is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details. You should have received a copy of
+ * the GNU General Public License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 package com.graphaware.module.noderank.utils;
 
+import com.google.common.base.Objects;
 import com.graphaware.common.util.Pair;
-import org.neo4j.graphdb.Node;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import com.google.common.base.Objects;
 
 /**
  * IndexNode pair, used for sorting the results of Page Rank algorithm
